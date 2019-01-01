6d2872613cf3ae1e9f2c06242d5c9c59328f0142
Manish <manishsahu53@Live.com>
2019-01-01 15:23:52 +0530

    corrected loss function

--- src/loss.py
+++ src/loss.py
@@ -19,13 +19,20 @@ def jaccard_distance(y_true, y_pred):
     return (1 - jac) * smooth
 
 
-def dice_coef(y_true, y_pred, smooth=10.0):
-    '''Average dice coefficient per batch.'''
-    axes = (1, 2, 3)
-    intersection = K.sum(y_true * y_pred, axis=axes)
-    summation = K.sum(y_true, axis=axes) + K.sum(y_pred, axis=axes)
+# def dice_coef(y_true, y_pred, smooth=10.0):
+#     '''Average dice coefficient per batch.'''
+#     axes = (1, 2, 3)
+#     intersection = K.sum(y_true * y_pred, axis=axes)
+#     summation = K.sum(y_true, axis=axes) + K.sum(y_pred, axis=axes)
 
-    return K.mean((2.0 * intersection + smooth) / (summation + smooth), axis=0)
+#     return K.mean((2.0 * intersection + smooth) / (summation + smooth), axis=0)
+
+
+def dice_coef(y_true, y_pred, smooth=10.0):
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
 
 
 def dice_coef_loss(y_true, y_pred):
@@ -35,6 +42,9 @@ def dice_coef_loss(y_true, y_pred):
 def jaccard_coef(y_true, y_pred, smooth=10.0):
     '''Average jaccard coefficient per batch.'''
     axes = (1, 2, 3)
-    intersection = K.sum(y_true * y_pred, axis=axes)
-    union = K.sum(y_true, axis=axes) + K.sum(y_pred, axis=axes) - intersection
-    return K.mean((intersection + smooth) / (union + smooth), axis=0)
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+
+    union = K.sum(y_true_f) + K.sum(y_pred_f) - intersection
+    return (intersection + smooth) / (union + smooth)
